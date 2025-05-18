2acfd648fa214df42848414c92f416d978e0599d
Michalikhol <michali.khol@gmail.com>
2025-05-18 20:08:20 +0300

    changed to material links

--- src/types/IPreparation.ts
+++ src/types/IPreparation.ts
@@ -2,5 +2,5 @@ export interface IPreparationResult {
     interview_id: string;
     company_info: string;
     job_info: string;
-    interview_questions: string[];
+    material_links: string[];
 }
